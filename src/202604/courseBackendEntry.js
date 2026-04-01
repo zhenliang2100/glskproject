b749a9731b873290de430937068d5e05557a2546
zhenliang2100 <zhenliang2100@gmail.com>
2026-04-01 15:50:40 +0800

    Add Tailwind CSS configuration and styles

--- src/202604/courseBackendEntry.js
+++ src/202604/courseBackendEntry.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '../index.css';
 import CourseBackendPage from './CourseBackendPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/202604/courseTabletViewEntry.js
+++ src/202604/courseTabletViewEntry.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '../index.css';
 import CourseTabletViewPage from './CourseTabletViewPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
