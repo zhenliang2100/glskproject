5cf28af871ca9791830b0574abd397617da3def3
zhenliang2100 <zhenliang2100@gmail.com>
2026-04-01 15:15:18 +0800

    Fix router: use HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import QwAssistantPage from './202604/QwAssistantPage';
 import CourseTabletViewPage from './202604/CourseTabletViewPage';
 import CourseBackendPage from './202604/CourseBackendPage';
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/glskproject">
+    <Router>
       <div className="App">
         <header className="App-header">
           <h1>GLSK Project - 202604</h1>
